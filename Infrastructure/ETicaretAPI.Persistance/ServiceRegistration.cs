8ae541b9a4906888b5a11835fd929ed99bbbfe7f
yasin çalışkan <yasin.clsk@hotmail.com>
2022-03-02 20:49:36 +0300

    2 April 2022

--- Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -1,11 +1,13 @@
 ﻿using ETicaretAPI.Application.Abstractions;
 using ETicaretAPI.Persistance.Concretes;
+using ETicaretAPI.Persistance.Contexts;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ETicaretAPI.Persistance
 {
@@ -13,7 +15,7 @@ namespace ETicaretAPI.Persistance
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
-            services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
